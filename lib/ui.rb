dd06c97ae895353cb56d1696c878a8a4b16a07f3
Jaxon Adams <jaxon.adams@loanpro.io>
2024-03-11 10:03:03 -0600

    created new UserInterface class

---
+++ lib/ui.rb
@@ -0,0 +1,8 @@
+# contains the class definition for the user interface, which allows
+#   the user to interact with this application through the CLI.
+
+class UserInterface
+    def display_title
+        puts "Hello, world!"
+    end
+end
--- main.rb
+++ main.rb
@@ -1,9 +1,12 @@
 # RecipeSync -- a GitHub -> Workato deployment tool for LoanPro Integrations
 # Author: Jaxon Adams
 
+require_relative "lib/ui.rb"
+
 module RecipeSync
     def self.main
-        puts "Hello, world!"
+        ui = UserInterface.new
+        ui.display_title
     end
 end
 
