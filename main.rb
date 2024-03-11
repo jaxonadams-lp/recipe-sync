5f0e08fa2b1b3c7479a46e1709db58f88b330731
Jaxon Adams <jaxon.adams@loanpro.io>
2024-03-11 08:56:40 -0600

    created RecipeSync module to encapsulate main app logic

---
+++ main.rb
@@ -0,0 +1,11 @@
+# RecipeSync -- a GitHub -> Workato deployment tool for LoanPro Integrations
+# Author: Jaxon Adams
+
+module RecipeSync
+    def self.main
+        puts "Hello, world!"
+    end
+end
+
+# !---------------------------------------------------------------------------
+RecipeSync.main()
