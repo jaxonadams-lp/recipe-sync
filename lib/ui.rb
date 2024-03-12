54638be9e007ed37804dcd70282bb53e6ba04fa6
Jaxon Adams <jaxon.adams@loanpro.io>
2024-03-12 10:54:42 -0600

    added method for collecting input and some validation logic

--- lib/ui.rb
+++ lib/ui.rb
@@ -9,6 +9,8 @@ class Menu
     end
 
     def print_menu
+        # print the available options to the screen, including an option
+        #  to exit. The exit option will always be option 0.
         puts @message
         puts "\n"
 
@@ -16,6 +18,17 @@ class Menu
             puts "[#{i + 1}] #{@options[i]}"
         end
         puts "[0] Exit"
+    end
+
+    def selection_is_valid?(selection)
+        # ensure that the given user input is a valid selection.
+        if selection.to_i.to_s == selection  # parsed to int and back should be the same value
+            as_int = selection.to_i
+            if as_int.between?(0, @options.length)
+                return true
+            end
+        end
+        return false
     end
 end
 
@@ -30,7 +43,19 @@ class UserInterface
     end
 
     def display_title
+        # prints some ascii art (defined in initialize) to the screen.
+
         puts @title
+    end
+
+    def collect_input(prompt)
+        # provides a way of collecting user input through a nicer display.
+
+        puts "\n"
+        print prompt
+        user_input = gets
+        puts "\n"
+        return user_input.chomp
     end
 
     def prompt_options(options)
@@ -38,5 +63,14 @@ class UserInterface
 
         menu = Menu.new("Pick an option.", options)
         menu.print_menu
+
+        selection = collect_input("  >> ")
+        puts "You selected option #{selection}"
+
+        if menu.selection_is_valid?(selection)
+            puts "Your selection was a valid option."
+        else
+            puts "Your selection was invalid."
+        end
     end
 end
