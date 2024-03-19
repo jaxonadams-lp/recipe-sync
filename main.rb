35f1c6751aaf15a3d3dd31d3e63796ce3ff70fad
Jaxon Adams <jaxon.adams@loanpro.io>
2024-03-19 14:32:54 -0600

    swapped recipesync module for a class def

--- main.rb
+++ main.rb
@@ -6,32 +6,20 @@ require_relative "lib/ghclient.rb"
 require_relative "lib/wkclient.rb"
 require_relative "config/config.rb"
 
-module RecipeSync
-    def self.deploy_python(ui)
-        settings = Configuration.new(ui, "./config/config.yaml")
+class RecipeSync
+    def initialize
+        @ui = UserInterface.new
+        @settings = Configuration.new(@ui, "./config/config.yaml")
 
-        access_token = settings.config["github"]["personal_access_token"]
-        gh_client = GitHubClient.new(ui, access_token)
+        gh_pat = @settings.config["github"]["personal_access_token"]
+        @gh_client = GitHubClient.new(@ui, gh_pat)
 
-        workato_key = settings.config["workato"]["api_key"]
-        wk_client = WorkatoClient.new(ui, workato_key)
-
-        owner = ui.prompt("Please enter the owner of your repository.")
-        name = ui.prompt("Please enter the repository name.")
-        path = ui.prompt("What file should I read? Please enter a relative path.")
-
-        code = gh_client.read_remote("#{owner}/#{name}", path)
-
-        # sample recipe id: 45459373 (dev workspace)
-        recipe_id = ui.prompt("Please enter the ID of the recipe you'd like to update.")
-        step = ui.prompt("Please enter the step number for your Python step.")
-
-        update_status = wk_client.update_code_step(recipe_id, step, code)
+        wk_key = @settings.config["workato"]["api_key"]
+        @wk_client = WorkatoClient.new(@ui, wk_key)
     end
 
-    def self.main
-        ui = UserInterface.new
-        ui.display_title
+    def main
+        @ui.display_title
 
         # menu of options -- each is a callable function
         option_map = {
@@ -39,12 +27,33 @@ module RecipeSync
         }
 
         # prompt user for what they'd like to do
-        selection = ui.prompt_options(option_map.keys)
+        selection = @ui.prompt_options(option_map.keys)
 
         # if not nil, call the function; else exit the program
-        option_map[selection] ? option_map[selection].(ui) : exit
+        option_map[selection] ? option_map[selection].() : exit
+    end
+
+    private
+
+    def deploy_python
+        # deploy a python script to one or many recipes in Workato
+
+        owner = @ui.prompt("Please enter the owner of your repository.")
+        name = @ui.prompt("Please enter the repository name.")
+        path = @ui.prompt("What file should I read? Please enter a relative path.")
+
+        code = @gh_client.read_remote("#{owner}/#{name}", path)
+
+        # sample recipe id: 45459373 (dev workspace)
+        recipe_id = @ui.prompt("Please enter the ID of the recipe you'd like to update.")
+        step = @ui.prompt("Please enter the step number for your Python step.")
+
+        update_status = @wk_client.update_code_step(recipe_id, step, code)
     end
 end
 
 # !---------------------------------------------------------------------------
-RecipeSync.main()
+app = RecipeSync.new
+
+# ... Engage!
+app.main()
